177fc908f5553a53dad3a196870843a49c03f48f
Tin <tin_nguyen95@hotmail.com>
2022-09-19 22:11:44 +0200

    feat: first api call try

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { catchError, map, Observable, of, startWith } from 'rxjs';
-import { FormControl } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {catchError, map, Observable, of} from 'rxjs';
+import {FormControl} from '@angular/forms';
+import {ApiService} from "../services/api-service";
+import {Construction} from "../../models/Construction";
 
 interface Category {
   value: string;
@@ -16,12 +18,17 @@ interface Category {
 export class MapComponent implements OnInit {
   apiLoaded$: Observable<boolean>;
 
-  categories: Category[] = [{ value: 'Est', color: 'red' }];
+  categories: Category[] = [{value: 'Est', color: 'red'}];
+  constructions$: Observable<Construction[]>;
   myControl = new FormControl('');
   options: string[] = ['One', 'Two', 'Three'];
-  filteredOptions: Observable<string[]>;
+  filteredOptions: Observable<Construction[]>;
+  googleMapsOptions: google.maps.MapOptions = {
+    center: {lat: 46.7739, lng: 8.6025},
+    zoom: 10,
+  };
 
-  constructor(httpClient: HttpClient) {
+  constructor(httpClient: HttpClient, private _apiService: ApiService) {
     this.apiLoaded$ = httpClient
       .jsonp(
         'https://maps.googleapis.com/maps/api/js?key=AIzaSyCmv7VOgTWeFT-dlUMxzaliANNR3AFO2e4',
@@ -33,23 +40,8 @@ export class MapComponent implements OnInit {
       );
   }
 
-  googleMapsOptions: google.maps.MapOptions = {
-    center: { lat: 46.7739, lng: 8.6025 },
-    zoom: 11,
-  };
-
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map((value) => this._filter(value || ''))
-    );
-  }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter((option) =>
-      option.toLowerCase().includes(filterValue)
-    );
+    this.constructions$ = this._apiService.getConstructions();
+    this.constructions$.subscribe(values => console.log(values));
   }
 }
