007c4294387d28d36f0c705ea9605f0cb3292df9
Tin <tin_nguyen95@hotmail.com>
2022-09-29 19:51:07 +0200

    feat: implement info page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,5 @@ export class AppComponent {
   title = 'ihs_interactive_map';
 
   contactOpened: boolean;
+  informationOpened: boolean;
 }
--- src/app/information/information.component.ts
+++ src/app/information/information.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-information',
   templateUrl: './information.component.html',
-  styleUrls: ['./information.component.css']
+  styleUrls: ['./information.component.scss']
 })
 export class InformationComponent implements OnInit {
+  @Output() closeEventEmitter: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  close() {
+    this.closeEventEmitter.emit()
   }
 
 }
